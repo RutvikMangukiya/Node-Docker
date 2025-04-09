cae5d22fa07104eb5e97b0c44ed1c312d5bde141
RutvikMangukiya <rutvikmangukiya007@gmail.com>
2025-04-09 17:49:45 +0530

    configure variables

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
 const express = require("express")
+const mongoose = require("mongoose");
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require("./config/config");
 
 const app = express();
+
+const mongoURl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?
+    authSource=admin`;
+
+const connectWithRetry = () => {
+    mongoose
+    .connect(mongoURl, {
+//      useNewUrlParser: true,
+//      useUnifiedTopology: true,
+//      useFindAndModify: false,
+    })
+    .then(() => console.log("Successfully connected to DB"))
+    .catch((e) => {
+        console.log(e)
+        setTimeout(connectWithRetry, 5000)
+    });
+}
+
+connectWithRetry()
 
 app.get("/", (req, res) => {
     res.send("<h2>Hello There...234..</h2>");
@@ -8,4 +29,4 @@ app.get("/", (req, res) => {
 
 const port = process.env.port || 3000;
 
-app.listen(port, () => console.log(`listening on port ${port}`)); 
+app.listen(port, () => console.log(`listening on port ${port}`));
